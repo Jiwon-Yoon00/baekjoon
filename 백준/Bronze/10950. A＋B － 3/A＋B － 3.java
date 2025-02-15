6423b62cbc73fd2a985d8e427d8274ca4535fdfc
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-02-16 03:21:38 +0900

    [Bronze V] Title: A+B - 3, Time: 104 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int tc = Integer.parseInt(br.readLine());
+
+
+        while (tc > 0){
+            String str =  br.readLine();
+            StringTokenizer st = new StringTokenizer(str, " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf(a + b) + "\n");
+            tc --;
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+}
