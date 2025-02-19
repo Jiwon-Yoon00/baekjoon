e589f3dc408d0aede5da437eb3d78db45d1f55fd
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-02-19 15:42:07 +0900

    [Bronze V] Title: N 찍기, Time: 180 ms, Memory: 18056 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -6,9 +6,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
+        StringBuilder sb = new StringBuilder();
         for(int i = 1; i <= n; i++){
-            System.out.println(i);
+            sb.append(i);
+            sb.append('\n');
         }
+
+        System.out.println(sb);
     }
 }
