7f52c64883337c549e8ce349e39aa0e3ab08cf70
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-02-19 16:14:26 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        if(n >= 90){
+            sb.append("A");
+        } else if (n >= 80) {
+            sb.append("B");
+        } else if (n >= 70) {
+            sb.append("C");
+        }else if (n >= 60) {
+            sb.append("D");
+        } else {
+            sb.append("F");
+        }
+        System.out.println(sb);
+    }
+}
