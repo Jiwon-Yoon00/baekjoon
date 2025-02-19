97dd82f13506744d49494c7408a4de2831e91a30
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-02-19 14:44:02 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(str+"??!");
+
+        System.out.println(sb);
+    }
+}
