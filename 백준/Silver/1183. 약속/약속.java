3c01811f2c2fb956a139d8e30fc28706d12c3a6e
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-07-27 01:59:12 +0900

    [Silver II] Title: 약속, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/1183. 약속/약속.java
+++ 백준/Silver/1183. 약속/약속.java
@@ -12,39 +12,22 @@ public class Main {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[][] arr = new int[N][2];
 		int[] waitTime = new int[N];
-		int maxDiff = 0;
 
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
-			waitTime[i] = arr[i][0] - arr[i][1];
+			int arrival = Integer.parseInt(st.nextToken());
+			int duration = Integer.parseInt(st.nextToken());
+			waitTime[i] = arrival - duration;
 		}
 
 		Arrays.sort(waitTime);
 
-		// 중앙값 구하기
-		int median;
-		if (N % 2 == 1) {
-			median = -waitTime[N / 2];
-		} else {
-			median = -waitTime[N / 2 - 1]; // 중앙값이 여러 개일 경우, 최소/최대 범위 구분 필요
-		}
-
-		// 최소 비용 계산
-		int minCost = 0;
-		for (int wt : waitTime) {
-			minCost += Math.abs(wt + median);
-		}
-
-		// 중앙값이 여러 개인 경우 최소 비용이 나오는 T의 개수를 세기
 		int count = 1;
 		if (N % 2 == 0) {
-			int median1 = -waitTime[N / 2 - 1];
-			int median2 = -waitTime[N / 2];
-			count = Math.abs(median2 - median1) + 1; // 모든 정수 T 중 최소 비용이 나오는 개수
+			int left = waitTime[N / 2 - 1];
+			int right = waitTime[N / 2];
+			count = right - left + 1;
 		}
 
 		System.out.println(count);
