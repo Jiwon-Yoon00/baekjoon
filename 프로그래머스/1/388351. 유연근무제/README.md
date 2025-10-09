88355b41bd12b41003f7fad498d1d975238a7aea
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-10-10 05:15:35 +0900

    [level 1] Title: 유연근무제, Time: 1.19 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/README.md
+++ 프로그래머스/1/388351. 유연근무제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 유연근무제 - 388351 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351#) 
 
 ### 성능 요약
 
-메모리: 91.4 MB, 시간: 1.10 ms
+메모리: 91.4 MB, 시간: 1.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 10일 04:59:19
+2025년 10월 10일 05:15:32
 
 ### 문제 설명
 
--- 프로그래머스/1/388351. 유연근무제/유연근무제.java
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -1,6 +1,40 @@
 class Solution {
     public int solution(int[] schedules, int[][] timelogs, int startday) {
+
+        // schedules 10분 더한 배열
+        int[] convertSchedules = new int[schedules.length];
+        for(int i = 0; i < schedules.length; i++){
+            int hour = schedules[i] / 100;
+            int minutes = schedules[i] % 100;
+            minutes += 10;
+            if(minutes >= 60){
+                hour += minutes / 60;
+                minutes %= 60;
+            }
+            convertSchedules[i] = hour * 100 + minutes;
+        }
+
         int answer = 0;
+        int day0 = startday - 1;  // 0~6 범위
+
+        for (int i = 0; i < schedules.length; i++) {
+            boolean success = true;
+            for (int j = 0; j < timelogs[i].length; j++) {
+
+                // j일 후 요일 계산
+                int dDay = (day0 + j) % 7;
+
+                // 토요일(5), 일요일(6) 건너뛰기
+                if(dDay == 5 || dDay == 6) continue;
+
+                if (convertSchedules[i] < timelogs[i][j]) {
+                    success = false;
+                    break;
+                }
+            }
+            if (success) answer++;
+        }
         return answer;
     }
 }
+
