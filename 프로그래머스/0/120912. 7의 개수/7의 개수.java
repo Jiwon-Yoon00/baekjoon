ac4a0dc27d1409360eb4d2cffdf2cf68fd695f8e
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-10-19 20:33:44 +0900

    [level 0] Title: 7의 개수, Time: 13.08 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -1,15 +1,7 @@
 class Solution {
     public int solution(int[] array) {
-        int answer = 0;
-        char target = '7';
-        for(int i =0; i < array.length; i++){
-            String str = String.valueOf(array[i]);
-            for(int j = 0; j < str.length(); j++){
-                if(str.charAt(j) == target){
-                    answer ++ ;
-                }
-            }
-        }
-        return answer;
+        String str = "";
+        for(int n:array) str += n;
+        return str.length() - str.replace("7", "").length();
     }
 }
