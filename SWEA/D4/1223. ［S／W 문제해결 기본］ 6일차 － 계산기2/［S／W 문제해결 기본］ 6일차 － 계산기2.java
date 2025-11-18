5d89dd26cce5c72721488d01be469e7ffc498573
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-11-18 21:23:43 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기2, Time: 86 ms, Memory: 24,960 KB -BaekjoonHub

--- SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.java
+++ SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.java
@@ -1,57 +1,64 @@
+
 import java.io.*;
 import java.util.*;
 
-class Solution {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Solution {
 
+
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new FileInputStream("src/codingTest/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+               
+        
         for (int test_case = 1; test_case <= 10; test_case++) {
             int N = Integer.parseInt(br.readLine());
             String expr = br.readLine();
-
+            String str = "";
             ArrayDeque<Character> opStack = new ArrayDeque<>();
-            StringBuilder postfix = new StringBuilder();
-
-            for (int i = 0; i < N; i++) {
-                char c = expr.charAt(i);
-                if (c == '+' || c == '*') {
-                    while (!opStack.isEmpty() && precedence(opStack.peek()) >= precedence(c)) {
-                        postfix.append(opStack.pop());
-                    }
-                    opStack.push(c);
-                } else { // 숫자
-                    postfix.append(c);
-                }
+            
+            for(int i = 0; i < N; i++) {
+            	char c = expr.charAt(i);
+            	//"34+56*+7+"
+            	if(c == '+' || c == '*') {
+            		while(!opStack.isEmpty() && (precedence(opStack.peek()) >= precedence(c))) {
+            			str += opStack.pop();
+            		}
+            		opStack.push(c);
+            	}else {
+            		str += c;
+            	}
             }
-
+            
             while (!opStack.isEmpty()) {
-                postfix.append(opStack.pop());
+                str += opStack.pop();
             }
-
-            // 2. 후위표기 계산
-            ArrayDeque<Integer> calc = new ArrayDeque<>();
-            for (int i = 0; i < postfix.length(); i++) {
-                char c = postfix.charAt(i);
-                if (c == '+') {
-                    int b = calc.pop();
-                    int a = calc.pop();
-                    calc.push(a + b);
-                } else if (c == '*') {
-                    int b = calc.pop();
-                    int a = calc.pop();
-                    calc.push(a * b);
-                } else {
-                    calc.push(c - '0'); 
-                }
+            
+            
+            // 계산
+            ArrayDeque<Integer> answer = new ArrayDeque<>();
+            for(int i = 0; i < str.length(); i ++) {
+            	char c = str.charAt(i);
+            	
+            	if(c == '+') {
+            		int a = answer.pop();
+            		int b = answer.pop();
+            		answer.push(a+b);
+            	}else if(c == '*') {
+            		int a = answer.pop();
+            		int b = answer.pop();
+            		answer.push(a*b);
+            	}else {
+            		answer.push(c - '0');
+            	}
             }
-
-            System.out.println("#" + test_case + " " + calc.pop());
+            System.out.println("#" + test_case + " " + answer.pop());
         }
     }
-
+    
     private static int precedence(char op) {
         if (op == '*') return 2;
         if (op == '+') return 1;
         return 0;
     }
+
 }
