e06eee29ab7ef640d54fe0702513f74d166c3119
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-12-01 21:26:44 +0900

    [Silver II] Title: 스택 수열, Time: 320 ms, Memory: 27636 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,42 +1,45 @@
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        int arr[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i ++){
+            arr[i] = Integer.parseInt(br.readLine()); 
         }
 
-        Stack<Integer> stack = new Stack<>();
-        int num = 1;
-        stack.push(num++);
-        sb.append("+").append("\n");
-        for (int i = 0; i < n; i++) {
-            if(!stack.isEmpty() && arr[i] < stack.peek() ){
-                System.out.println("NO");
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+       
+        int x = 1;
+        int index = 0;
+        
+        while(true){
+            if(!stack.isEmpty() && stack.peek() == arr[index]){
+                stack.pop();
+                //System.out.println("-");
+                sb.append("-").append("\n");
+                index++;
+            }else{
+                stack.push(x);
+                sb.append("+").append("\n");
+                //System.out.println("+");
+                x++;   
+            }
+
+            if(stack.isEmpty() && index == N){
+                System.out.println(sb);
                 break;
             }
 
-            while(true){
-
-                if(!stack.isEmpty() && arr[i] == stack.peek()){
-                    stack.pop();
-                    sb.append("-").append("\n");
-                    break;
-                }else {
-                    stack.push(num++);
-                    sb.append("+").append("\n");
-                }
+            if(!stack.isEmpty() && stack.peek() > arr[index]){
+                System.out.println("NO");
+                break;
             }
-        }
-        if(stack.isEmpty()){
-            System.out.println(sb);
         }
     }
 }
