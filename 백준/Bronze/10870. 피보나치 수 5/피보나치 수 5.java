647510c27f347c0bc8e07c0045539dab162c61cc
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2026-01-27 01:42:18 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        if(N == 0){
+            System.out.println(0);
+            return;
+        }else if(N == 1){
+            System.out.println(1);
+            return;
+        }
+        
+        int[] dp = new int[N + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= N; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+       
+        System.out.println(dp[N]);
+        
+    }
+}
