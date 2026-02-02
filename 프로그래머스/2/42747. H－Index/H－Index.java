0cb221851a1e641045b547cf67731c1c55e23ceb
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2026-02-02 23:26:58 +0900

    [level 2] Title: H-Index, Time: 0.81 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+     
+        int len = citations.length;
+        // 0 1 3 5 6
+        for(int i = 0; i < len; i++){
+            int h = len - i;
+            if(citations[i] >= h){
+                answer = h;
+                break;
+            }
+        }
+        return answer;
+    }
+}
