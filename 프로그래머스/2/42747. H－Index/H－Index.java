ae17a1ee79a71a9e11b6e08dddb8d6af430f71f6
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2026-02-08 21:35:10 +0900

    [level 2] Title: H-Index, Time: 0.78 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,17 +3,17 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
+        int n = citations.length;
         Arrays.sort(citations);
-     
-        int len = citations.length;
-        // 0 1 3 5 6
-        for(int i = 0; i < len; i++){
-            int h = len - i;
-            if(citations[i] >= h){
+
+        for (int i = 0; i < n; i++) {
+            int h = n - i;
+            if (citations[i] >= h) { 
                 answer = h;
                 break;
             }
         }
+
         return answer;
     }
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 72.7 MB, 시간: 0.81 ms
+메모리: 76.2 MB, 시간: 0.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 02일 23:26:56
+2026년 02월 08일 21:35:07
 
 ### 문제 설명
 
