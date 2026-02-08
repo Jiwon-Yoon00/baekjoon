8c4f6b586677252d09fec82f859d1ff36af2f5ba
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2026-02-08 20:29:38 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,22 +4,12 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        for(int f = 0; f < commands.length; f++){
-            int i = commands[f][0];
-            int j = commands[f][1];
-            int k = commands[f][2];
-            
-            int[] temp = new int[j - i + 1];
-            int idx = 0;
-            for(int s = i; s <= j; s++){
-                temp[idx++] = array[s-1]; 
-            }
-            idx = 0;
-            Arrays.sort(temp);
-            
-            answer[f] = temp[k-1];
-            
+        for(int i = 0; i < commands.length ; i++){
+            int[] newArray = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(newArray);
+            answer[i] = newArray[commands[i][2] - 1];
         }
+        
         return answer;
     }
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 80.3 MB, 시간: 0.44 ms
+메모리: 79.9 MB, 시간: 0.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 02일 19:23:44
+2026년 02월 08일 20:29:36
 
 ### 문제 설명
 
