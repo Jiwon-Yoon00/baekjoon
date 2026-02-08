b1d464e6b2bb23528411595bc325c0c34ed35173
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2026-02-08 21:50:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 90.87 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,25 +3,21 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        //map에 있는 지 없는 지 확인.
-        
         HashMap<String, Integer> map = new HashMap<>();
-
-        for(String p : participant){
-            map.put(p, map.getOrDefault(p, 0) + 1);
+        
+        for(int i = 0; i < participant.length; i++){
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
         }
         
-        for (String c : completion) {
-            map.put(c, map.get(c) - 1);
+        
+        for(int i = 0; i < completion.length; i++){
+            // map에서 key 값이 가지고 있는 value에서 1씩 뺴기
+            map.put(completion[i], map.get(completion[i]) - 1);
         }
         
-        for(String key: map.keySet()){
-            if(map.get(key) > 0){
-                return key;
-            }
+        for (String key : map.keySet()) {
+            if (map.get(key) > 0) answer = key;
         }
-        
-        return "";
-        
+        return answer;
     }
 }
